013a60d41a28710cce2a39c7692332ca18e9819a
Cam Nguyen Tran <73975520+cnmeow@users.noreply.github.com>
2023-04-19 22:41:25 +0700

    Create BytesAndBigIntegers.py

--- General/Encoding/BytesAndBigIntegers.py
+++ General/Encoding/BytesAndBigIntegers.py
@@ -1,4 +1,4 @@
-!pip3 install PyCryptodome # Cài đặt thư viện PyCryptodome
+# !pip3 install PyCryptodome # Cài đặt thư viện PyCryptodome
 from Crypto.Util.number import long_to_bytes
 longnum = 11515195063862318899931685488813747395775516287289682636499965282714637259206269
 # Chuyển long thành bytes để lấy flag
