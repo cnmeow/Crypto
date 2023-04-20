6b501df50ecb95ec23ddba95e12e7ffdf4f14cce
Cam Nguyen Tran <73975520+cnmeow@users.noreply.github.com>
2023-04-20 08:08:14 +0700

    Create Transparency.py

---
+++ General/DataFormats/Transparency.py
@@ -0,0 +1,3 @@
+# Dùng https://www.nmmapper.com/sys/tools/subdomainfinder/ để tìm tất cả subdomain của 'cryptohack.org'
+# Thử vào mọi submain, trong đó có https://thetransparencyflagishere.cryptohack.org/ là chứa flag
+print('crypto{thx_redpwn_for_inspiration}')
