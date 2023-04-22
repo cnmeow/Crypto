02681573876f45928445c148bdbb680af8f97caa
Cam Nguyen Tran <73975520+cnmeow@users.noreply.github.com>
2023-04-22 23:07:23 +0700

    Create ModesOfOperationStarter.py

---
+++ SymmetricCiphers/SymmetricStarter/ModesOfOperationStarter.py
@@ -0,0 +1,6 @@
+# Encrypt_flag() và nhận được ciphertext
+# ciphertext = 'ffb746c1581384ac1ce348483878a69e535a2c819b9cc90a1b56d9f511065d91'
+
+# Decrypt(ciphertext) và nhận được plaintext
+plaintext = '63727970746f7b626c30636b5f633170683372355f3472335f663435375f217d'
+print(bytes.fromhex(plaintext))
